1bb5f990fa4a95ff84778e939a851a95e2741f06
wuhaowen <657877500@qq.com>
2022-03-19 20:08:11 +0800

    ver1.0

---
+++ src/main/java/com/whw/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.whw.git;
+
+/**
+ * @Author 吴浩文
+ * @Date 2022/3/19 20:06
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,hxx");
+    }
+}
